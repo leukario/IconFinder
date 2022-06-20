277844d32526181bd6714c677b10ae4d02c93604
Lokesh <bishtlokesh000@gmail.com>
2022-06-20 15:30:23 +0530

    Merge branch 'main' of github.com:leukario/IconFinder

--- app/src/main/java/com/example/iconfinder/adapter/Iconlistadapter.kt
+++ app/src/main/java/com/example/iconfinder/adapter/Iconlistadapter.kt
@@ -2,25 +2,26 @@ package com.example.iconfinder.adapter
 
 
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.net.wifi.WifiConfiguration.AuthAlgorithm.strings
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
-import com.example.iconfinder.BuildConfig
 import com.example.iconfinder.R
-import com.example.iconfinder.model.Category
 import com.example.iconfinder.model.Icon
-import com.example.iconfinder.network.BASE_URL
-import com.example.iconfinder.network.askForPermission
-import com.example.iconfinder.network.downloadImage
-import com.example.iconfinder.network.isPermissionGranted
+import com.example.iconfinder.network.*
 
 
 import kotlinx.android.synthetic.main.grid_item.view.*
+import java.io.File
 
 class IconListAdapter(var list: List<Icon>)
     : RecyclerView.Adapter<IconListAdapter.ViewHolder>() {
@@ -49,7 +50,7 @@ class IconListAdapter(var list: List<Icon>)
 
 
             with(itemView) {
-                   icon_name.text = "${item.categories[0].name}".capitalize()
+                   icon_name.text = "${item.categories[0].name}"
 
                 if (item.raster_sizes.size > 6)
                     image.load(item.raster_sizes[6].formats[0].preview_url) {
@@ -63,7 +64,7 @@ class IconListAdapter(var list: List<Icon>)
                         placeholder(R.drawable.ic_loading)
                     }
 
-                if (item.is_premium) { // Show Price
+                if (item.is_premium) {
                     image_paid.visibility = View.VISIBLE
                     download_btn.visibility = View.GONE
                     price.visibility = View.VISIBLE
@@ -75,18 +76,45 @@ class IconListAdapter(var list: List<Icon>)
                     image_paid.visibility = View.INVISIBLE
                     download_btn.visibility = View.VISIBLE
                     price.visibility = View.INVISIBLE
-                    setButtonClick(item)
+                  download_btn.setOnClickListener {
+                        if (!isPermissionGranted(context)) {
+                            requestPermissions(context as Activity,
+                                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                                2
+                            )
+                        } else {
+                          //  Log.d("else","Request Accepted")
+                            val filePath = item.raster_sizes[0].formats[0].download_url
+                          //  Log.d("else",filePath.toString())
+                           download(context, filePath,item.categories[0].name)
+                        }
+                    }
                     }
                 }
             }
-        private fun View.setButtonClick(item: Icon) {
-            download_btn.setOnClickListener {
-                if (!isPermissionGranted(context)) {
-                    askForPermission(context as Activity)
-                } else {
-                    val filePath = item.raster_sizes[0].formats[0].download_url
-                    downloadImage(context, filePath)
+        private fun download(context: Context, url:String, fileName:String) {
+            if (isPermissionGranted(context)) {
+                try {
+                    val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                    val imageLink = Uri.parse(url)
+                    val request = DownloadManager.Request(imageLink)
+                    request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
+                        .setMimeType("image/png")
+                        .setAllowedOverRoaming(false)
+                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                        .setTitle(fileName)
+                        .addRequestHeader("Authorization", "Bearer X0vjEUN6KRlxbp2DoUkyHeM0VOmxY91rA6BbU5j3Xu6wDodwS0McmilLPBWDUcJ1")
+                        .setDestinationInExternalPublicDir(
+                            Environment.DIRECTORY_PICTURES,
+                            File.separator + fileName + ".png"
+                        )
+                    downloadManager.enqueue(request)
+                    context.toast("Icon Download Successful")
+                } catch (e: Exception) {
+                    context.toast("Icon Download Failed")
                 }
+            } else {
+                askForPermission(context as Activity)
             }
         }
     }
